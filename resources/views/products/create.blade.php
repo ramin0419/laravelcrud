abe5c1da1ee63d2226518cc61dc3e54c5ca63178
Ramin <ramin.shrestha057@gmail.com>
2023-07-20 09:04:16 +0545

    complete

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('main')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-sm-8">
+      <div class="card mt-3 p-3">
+        <Form method="POST" action="/products/store" enctype="multipart/form-data">
+          @csrf
+          <div class="form-group">
+            <label>Name</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name')}}">
+            @if($errors->has('name'))
+            <span class="text-danger">{{$errors->first('name')}}
+            </span>
+            @endif
+          </div>
+          <div class="form-group">
+            <label>Description</label>
+            <Textarea class="form-control" name="description" rows="4">{{ old('description')}}</Textarea>
+            @if($errors->has('description'))
+            <span class="text-danger">{{$errors->first('description')}}
+            </span>
+            @endif
+          </div>
+          <div class="from-group">
+            <label>Image</label>
+            <input type="file" name="image" class="form-control">
+            @if($errors->has('image'))
+            <span class="text-danger">{{$errors->first('image')}}
+            </span>
+            @endif
+          </div>
+          <button type="submit" class="btn btn-dark">Submit</button>
+        </Form>
+      </div>
+    </div>
+  </div>
+
+</div>
+@endsection
